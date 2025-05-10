628bad79aeeba9a96abfde16396ca9b30da75f6e
Teodor Patsov <jarousqui@icloud.com>
2025-05-10 10:12:15 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from datasets import load_dataset
 
+import pandas as pd
+from matplotlib import pyplot as plt
+
 # Login using e.g. `huggingface-cli login` to access this dataset
 ds = load_dataset("ns2agi/antwerp-osm-navigator")
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    print(ds)
+    print(ds['train'][0])  # prints first row
+    print(ds['train'].features)  # prints types and structure
+
+    sample = ds['train'].shuffle(seed=42).select(range(1000))
+    df = pd.DataFrame(sample)
+    print(df.head())
+    print(df.columns)
+
+    plt.scatter(df['lon'], df['lat'], s=1, alpha=0.5)
+    plt.title("Map of OSM Points in Antwerp")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.show()
