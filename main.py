01084a7fb436549b171d1cb1f3eb884e9e240a90
Teodor Patsov <jarousqui@icloud.com>
2025-05-10 09:57:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from datasets import load_dataset
+
+# Login using e.g. `huggingface-cli login` to access this dataset
+ds = load_dataset("ns2agi/antwerp-osm-navigator")
+
+if __name__ == "__main__":
+    print("Hello, World!")
